1215c489fc09680c2ae1ceffd12ffc790a17e85e
b3ktas <b3ktas.pro@gmail.com>
2026-06-06 04:04:49 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,8 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Intro />
       <Collections />
+      <Intro />
       <BestSellers />
       <Presentation />
       <Catalogue />
